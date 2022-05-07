aa11db06eef0e274a07449bdee174708e31f49e4
amcmanu3 <mcdeweykp@gmail.com>
2022-05-07 13:40:43 -0400

    Add Ubuntu 22.04 Support

--- install_crafty.py
+++ install_crafty.py
@@ -70,6 +70,10 @@ def do_distro_install(distro):
         pretty.info("We are updating python3, open-jdk and pip")
         script = os.path.join(real_dir, "app", "ubuntu_21_10.sh")
 
+    elif distro == "ubuntu_22_04.sh":
+        pretty.info("We are updating python3, open-jdk and pip")
+        script = os.path.join(real_dir, "app", "ubuntu_22_04.sh")
+
     elif distro == "pop_18_04.sh":
         pretty.info("We are updating python3, open-jdk and pip")
         script = os.path.join(real_dir, "app", "pop_18_04.sh")
@@ -407,9 +411,12 @@ def get_distro():
         elif version == "21.10":
             logger.info("Ubuntu 21.10 Detected")
             file = "ubuntu_21_10.sh"
+        elif version == "22.04":
+            logger.info("Ubuntu 22.04 Detected")
+            file = "ubuntu_22_04.sh"
         else:
             logger.critical(
-                "Unsupported Ubuntu - We only support Ubuntu 18.04 / 20.04 / 20.10 / 21.04 / 21.10"
+                "Unsupported Ubuntu - We only support Ubuntu 18.04 / 20.04 / 20.10 / 21.04 / 21.10 / 22.04"
             )
 
     elif id == "centos":
